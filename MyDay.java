40c1956402793ec47cc9162f2cc231d8d70ebd44
ahedlund15 <ahedlund15@punahou.edu>
2015-04-07 11:54:01 -1000

    made monthDays to get the number of days before the month that is inputed and made an equation to figure out the number of days so that i can place what week day it is

--- MyDay.java
+++ MyDay.java
@@ -26,7 +26,7 @@ public class MyDay
     }
     else yearDays = 365;
     
-    tDay = (year - startYear) * yearDays;
+    tDay = ((year - startYear) * yearDays) + monthDays() + day;
     
     return weekNames[0];    //0 just a placeholder rn
   }
@@ -35,11 +35,11 @@ public class MyDay
   {                          //important bc 365 vs. 366 days
     boolean leap = false;
     int year = Integer.parseInt(myDate.substring(6,10));
-    if (year % 4 == 0)
+    if (year % 4 == 0)  //leap years are divisible by 4
     {
-      if (year % 100 == 0)
+      if (year % 100 == 0) //but if it's also divisible by 100 it's not
       {
-        if (year % 400 == 0)
+        if (year % 400 == 0) //unless it's also divisible by 400
         {
           leap = true;
         }
@@ -53,7 +53,29 @@ public class MyDay
   
   public int monthDays()
   {
-    int numbDays = 0;
+    int fullMonth = 0;
+    int totalDays = 0;
+    int month = Integer.parseInt(myDate.substring(3,5));
+    int preMonths = month - 1;
+    if (preMonths == 0) month = 0;
+    else if (preMonths > 0)
+    {
+      if (preMonths == 1) totalDays = 31;
+      if (preMonths == 2) totalDays = 59;
+      if (preMonths == 3) totalDays = 90;
+      if (preMonths == 4) totalDays = 120;
+      if (preMonths == 5) totalDays = 151;
+      if (preMonths == 6) totalDays = 181;
+      if (preMonths == 7) totalDays = 212;
+      if (preMonths == 8) totalDays = 243;
+      if (preMonths == 9) totalDays = 273;
+      if (preMonths == 10) totalDays = 304;
+      if (preMonths == 11) totalDays = 334;
+    }
+    if (leapYear() == true) totalDays++;
+    return totalDays;
+    /*int numbDays = 0;
+    int myMonthDays = 0;
     int month = Integer.parseInt(myDate.substring(3,5));
     if ((month == 1) ||
         (month == 3) ||
@@ -80,6 +102,7 @@ public class MyDay
       }
       else numbDays = 28;
     }
-    return numbDays;
+    myMonthDays = 
+    return myMonthDays;*/
   }
 }
